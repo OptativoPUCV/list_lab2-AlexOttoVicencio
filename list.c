a4dd88b7ec31b03e86bb9b8f22e3cbef32530881
Replit user <replituser@example.com>
2021-03-29 23:19:10 +0000

    new try

--- list.c
+++ list.c
@@ -121,7 +121,9 @@ void * popCurrent(List * list) {
   if(list->current==list->head){
       aux=list->head;
       list->head=list->head->next;
+      
       free(list->head->prev);
+      list->head->prev=NULL;
       return (void *)aux->data;
   }
   if(list->current==list->tail){
